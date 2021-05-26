6fb9b3b2caa57977261dd64d583046de074b5740
Yensi Manuel De Luna Martinez <yensi_deluna@ucne.edu.do>
2021-05-26 17:52:59 -0400

    Listo

---
+++ RegistroPersonas/Migrations/20210526212749_MigracionPrestamos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistroPersonas.Migrations
+{
+    public partial class MigracionPrestamos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Balance",
+                table: "Persona",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.CreateTable(
+                name: "Prestamo",
+                columns: table => new
+                {
+                    PrestamoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    PersonaId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Concepto = table.Column<string>(type: "TEXT", nullable: true),
+                    Monto = table.Column<float>(type: "REAL", nullable: false),
+                    Balance = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestamo", x => x.PrestamoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prestamo");
+
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Persona");
+        }
+    }
+}
